d682a01aa50bed1c2b9b20989b749966350ab61f
Bruno <bru_aba89@hotmail.com>
2024-08-15 16:46:06 -0300

    primer commit

---
+++ src/App.tsx
@@ -0,0 +1,124 @@
+import { useCallback, useEffect, useState } from "react";
+import { HangmanDrawing } from "./HangmanDrawing";
+import { HangmanWord } from "./HangmanWord";
+import { Keyboard } from "./Keyboard";
+import words from "./wordList.json";
+
+// Función para eliminar tildes
+const removeAccents = (str: string) => {
+  let normalizedStr = str.normalize('NFD');
+  normalizedStr = normalizedStr.replace(/[\u0300-\u036f]/g, '');
+  return normalizedStr;
+};
+
+function getWord() {
+  const word = words[Math.floor(Math.random() * words.length)];
+  return removeAccents(word);
+}
+
+function App() {
+  const [wordToGuess, setWordToGuess] = useState(getWord);
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
+
+  const incorrectLetters = guessedLetters.filter(
+    letter => !wordToGuess.includes(letter)
+  );
+
+  const isLoser = incorrectLetters.length >= 6;
+  const isWinner = wordToGuess
+    .split("")
+    .every(letter => guessedLetters.includes(letter));
+
+  const addGuessedLetter = useCallback(
+    (letter: string) => {
+      if (guessedLetters.includes(letter) || isLoser || isWinner) return;
+
+      setGuessedLetters(currentLetters => [...currentLetters, letter]);
+    },
+    [guessedLetters, isWinner, isLoser]
+  );
+
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      const key = e.key;
+      if (!key.match(/^[a-z]$/)) return;
+
+      e.preventDefault();
+      addGuessedLetter(key);
+    };
+
+    document.addEventListener("keypress", handler);
+
+    return () => {
+      document.removeEventListener("keypress", handler);
+    };
+  }, [guessedLetters]);
+
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      const key = e.key;
+      if (key !== "Enter") return;
+
+      e.preventDefault();
+      setGuessedLetters([]);
+      setWordToGuess(getWord());
+    };
+
+    document.addEventListener("keypress", handler);
+
+    return () => {
+      document.removeEventListener("keypress", handler);
+    };
+  }, []);
+
+  return (
+    <div
+      style={{
+        position: "relative", // Necesario para el contenedor padre
+        maxWidth: "800px",
+        display: "flex",
+        flexDirection: "column",
+        gap: "2rem",
+        margin: "0 auto",
+        alignItems: "center",
+      }}
+    >
+      <div
+        style={{
+          position: "absolute",
+          top: "50%", // Ajusta esto según tus necesidades
+          right: "-200px", // Cambia esto para ajustar la posición horizontal
+          transform: "translateY(-50%)", // Centra verticalmente
+          fontSize: "2rem",
+          textAlign: "center",
+          width: "200px", // Ajusta el ancho si es necesario
+          backgroundColor: "white", // Opcional, para un fondo visible
+          padding: "1rem",
+          zIndex: 10, // Asegúrate de que esté sobre otros elementos
+        }}
+      >
+        {isWinner && "Ganaste! - Refresca la página para jugar de nuevo"}
+        {isLoser && "Perdiste! - Refresca la página para jugar de nuevo"}
+      </div>
+      <HangmanDrawing numberOfGuesses={incorrectLetters.length} />
+      <HangmanWord
+        reveal={isLoser}
+        guessedLetters={guessedLetters}
+        wordToGuess={wordToGuess}
+      />
+      <div style={{ alignSelf: "stretch" }}>
+        <Keyboard
+          disabled={isWinner || isLoser}
+          activeLetters={guessedLetters.filter(letter =>
+            wordToGuess.includes(letter)
+          )}
+          inactiveLetters={incorrectLetters}
+          addGuessedLetter={addGuessedLetter}
+        />
+      </div>
+    </div>
+  )
+  
+}
+
+export default App;
